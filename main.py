42925fcc3302783e70f3e78cb95924d60e857c34
jepetolee <55366212+jepetolee@users.noreply.github.com>
2021-12-06 14:12:38 +0900

    Make main_UI

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
+#import crypto packages
+import ccxt as crypto_main
+import pyupbit as upbit
+
+#import sys
 import sys
+
+#import binance from ccxt
+binance = crypto_main.binance()
+
+#GUI
 from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
 
 
-class MyApp(QWidget):
+#UI design
+class CRYPTONALYTICS(QWidget):
 
-    def __init__(self):
-        super().__init__()
-        self.initUI()
+  def __init__(self):
+      super().__init__()
+      self.initUI()
 
-    def initUI(self):
-        self.setWindowTitle('My First Application')
-        self.move(300, 300)
-        self.resize(400, 200)
-        self.show()
+  def initUI(self):
+      self.setWindowTitle('CRYPTONALYTICS')
+      self.setWindowIcon(QIcon('bitcoin.png'))
+      self.setGeometry(400, 200, 900, 600)
+
+      menu = self.
+
+      self.show()
+
 
 
 if __name__ == '__main__':
-   app = QApplication(sys.argv)
-   ex = MyApp()
-   sys.exit(app.exec_())
+    #start_line
+  app = QApplication(sys.argv)
+  ex = CRYPTONALYTICS()
+  sys.exit(app.exec_())
