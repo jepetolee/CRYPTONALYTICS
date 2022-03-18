386ee2acee856ee52a8a69c41d2e6f599dc78a48
jepetolee <jepetolee@gmail.com>
2022-03-18 15:46:53 +0900

    fixed

--- prophet/__init__.py
+++ prophet/__init__.py
@@ -4,5 +4,3 @@ from .fifteen_minute_prophet import *
 from .one_hour_prophet import *
 from .four_hour_prophet import *
 from .one_day_prophet import *
-from .three_day_prophet import *
-from .one_week_prophet import *
