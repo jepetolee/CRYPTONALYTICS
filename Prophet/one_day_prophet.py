3c7a064b6630d7e709700270af5a2df529ee5ff6
jepetolee <jepetolee@gmail.com>
2022-03-19 20:03:35 +0900

    added

--- Prophet/one_day_prophet.py
+++ Prophet/one_day_prophet.py
@@ -10,7 +10,6 @@ from make_pd import future_symbol_daily_data
 
 def FutureOneDayVariancePercent():
     array = []
-    symbolic = []
     symbol = ['BTCUSDT',
               'ETHUSDT',
               'BNBUSDT',
@@ -24,17 +23,39 @@ def FutureOneDayVariancePercent():
               'LTCUSDT',
               'NEARUSDT']
     for _i in range(len(symbol)):
+
         data = future_symbol_daily_data(symbol[_i])
         if data is not None:
-            data = cal_varience_percent(data['2'].to_numpy())
+            data = cal_varience_percent(data['4'].to_numpy())
+
             array.append(data)
-            symbolic.append(symbol[_i])
-    return array, symbolic
+    return array
+
+
+def FutureOneDayData():
+    array = []
+    symbol = ['BTCUSDT',
+              'ETHUSDT',
+              'BNBUSDT',
+              'SOLUSDT',
+              'XRPUSDT',
+              'LUNAUSDT',
+              'BCHUSDT',
+              'WAVESUSDT',
+              'TRXUSDT',
+              'AVAXUSDT',
+              'LTCUSDT',
+              'NEARUSDT']
+    for _i in range(len(symbol)):
+
+        data = future_symbol_daily_data(symbol[_i])
+        if data is not None:
+            array.append(data.to_numpy())
+    return array
 
 
 def FutureOneDaylog():
     arrays = []
-    symbolic = []
     symbol = ['BTCUSDT',
               'ETHUSDT',
               'BNBUSDT',
@@ -50,15 +71,13 @@ def FutureOneDaylog():
     for _i in range(len(symbol)):
         data = future_symbol_daily_data(symbol[_i])
         if data is not None:
-            data = log(data['2'].to_numpy())
+            data = log(data['4'].to_numpy())
             arrays.append(data)
-            symbolic.append(symbol[_i])
-    return arrays, symbolic
+    return arrays
 
 
 def FutureProphetOneDay():
     arrays = []
-    symbolic = []
     symbol = ['BTCUSDT',
               'ETHUSDT',
               'BNBUSDT',
@@ -75,21 +94,18 @@ def FutureProphetOneDay():
         data = future_symbol_daily_data(symbol[_i])
         if data is not None:
             prophet = NeuralProphet(seasonality_mode='multiplicative', n_forecasts=14, epochs=300, n_changepoints=200)
-            data = DataFrame({'ds': data.index, 'y': data['2'].copy()})
+            data = DataFrame({'ds': data.index, 'y': data['4'].copy()})
 
             prophet.fit(data, validation_df=data, freq="D")
             future = prophet.make_future_dataframe(data, periods=14)
             df = prophet._prepare_dataframe_to_predict(future)
             dates, predicted, components = prophet._predict_raw(df, include_components=True)
-
             arrays.append(predicted[-14])
-            symbolic.append(symbol[_i])
-    return arrays, symbolic
+    return arrays
 
 
 def FutureOneDayRsi():
     arrays = []
-    symbolic = []
     symbol = ['BTCUSDT',
               'ETHUSDT',
               'BNBUSDT',
@@ -105,15 +121,14 @@ def FutureOneDayRsi():
     for _i in range(len(symbol)):
         data = future_symbol_daily_data(symbol[_i])
         if data is not None:
-            data = ta._ta_lib.RSI(data['3'].to_numpy(), 14)
+            data = ta._ta_lib.RSI(data['4'].to_numpy(), 14)
+
             arrays.append(data)
-            symbolic.append(symbol[_i])
-    return arrays, symbolic
+    return arrays
 
 
 def FutureOneDayMacd():
-    arrays = []
-    symbolic = []
+    upper, middles = list(), list()
     symbol = ['BTCUSDT',
               'ETHUSDT',
               'BNBUSDT',
@@ -129,14 +144,16 @@ def FutureOneDayMacd():
     for _i in range(len(symbol)):
         data = future_symbol_daily_data(symbol[_i])
         if data is not None:
-            data = ta._ta_lib.MACD(data['3'].to_numpy(), 14)
-            arrays.append(data)
-            symbolic.append(symbol[_i])
-    return arrays, symbolic
+            up = ta._ta_lib.SMA(data['4'].to_numpy(), 3)
+            middle = ta._ta_lib.SMA(data['4'].to_numpy(), 14)
+            upper.append(up)
+            middles.append(middle)
+
+    return upper, middles
 
 
 def FutureOneDayBBands():
-    upper, middles, lows, symbolic = list(), list(), list(), list()
+    upper, middles, lows = list(), list(), list()
     symbol = ['BTCUSDT',
               'ETHUSDT',
               'BNBUSDT',
@@ -152,11 +169,8 @@ def FutureOneDayBBands():
     for _i in range(len(symbol)):
         data = future_symbol_daily_data(symbol[_i])
         if data is not None:
-            up, middle, low = ta._ta_lib.BBANDS(data['3'].to_numpy(), 20, 2, 2, matype=MA_Type.SMA)
+            up, middle, low = ta._ta_lib.BBANDS(data['4'].to_numpy(), 14, 2, 2, matype=MA_Type.SMA)
             upper.append(up)
             middles.append(middle)
             lows.append(low)
-            symbolic.append(symbol[_i])
-    return upper, middles, lows, symbolic
-
-
+    return upper, middles, lows
--- TradeAlgorithm/BuildTradeDataSet.py
+++ TradeAlgorithm/BuildTradeDataSet.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from Prophet import *
 
 class DatasetBuilder:
 
@@ -7,27 +7,49 @@ class DatasetBuilder:
         label = data.shape[0]
         samples = int((label - input_data - output) // stride) + 1
 
-        X = np.zeros(([input_data, samples]))
-        Y = np.zeros([output, samples])
-
-        for i in np.arrange(samples):
+        X = np.zeros(([samples, input_data, 13]))
+        Y = np.zeros([samples, output])
+        for i in range(samples):
             # build data set fot X, Y
             startx = stride * i
             endx = startx + input_data
-            X[:, i] = data[startx:endx]
+
+            X[i] = data[startx:endx]
 
             starty = stride * i + input_data
             endy = starty + output
-            Y[:, i] = data[starty:endy]
-
-        X = X.reshape(X.shape[0], X.shape[1], 1).transpose((1, 0, label))
-        Y = Y.reshape(Y.shape[0], Y.shape[1], 1).transpose((1, 0, label))
+            Y[i] = data.T[3][starty:endy]
         self.x = X
         self.y = Y
-        self.len = len(X)
 
     def __getitem__(self, item):
         return self.x[item], self.y[item]
 
-    def __len__(self):
-        return self.len
+
+def OneDayDataSetOut():
+    # prophet = FutureProphetOneDay()
+    data = FutureOneDayData()
+    mupper, mmiddles = FutureOneDayMacd()
+
+    data_bundle = list()
+    rsi = FutureOneDayRsi()
+    upper, middles, lows = FutureOneDayBBands()
+    logs, variance = FutureOneDaylog(), FutureOneDayVariancePercent()
+    for i in range(len(data)):
+        dataset = np.vstack([data[i][14:].T, rsi[i][14:]])
+        dataset = np.vstack([dataset, mupper[i][14:]])
+        dataset = np.vstack([dataset, mmiddles[i][14:]])
+        dataset = np.vstack([dataset, logs[i][14:]])
+        dataset = np.vstack([dataset, upper[i][14:]])
+        dataset = np.vstack([dataset, middles[i][14:]])
+        dataset = np.vstack([dataset, lows[i][14:]])
+        dataset = np.vstack([dataset, variance[i][13:]]).T
+
+        dataset = DatasetBuilder(dataset, 14, 3, 4)
+        data_bundle.append(dataset)
+
+    return data_bundle
+
+
+if __name__ == "__main__":
+    OneDayDataSetOut()
