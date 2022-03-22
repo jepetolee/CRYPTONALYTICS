91b3df8e34d004150117795060905bdb84730bc9
jepetolee <jepetolee@gmail.com>
2022-03-22 22:59:52 +0900

    Update train.py

--- TrainingModel/train.py
+++ TrainingModel/train.py
@@ -5,13 +5,14 @@ from TradeAlgorithm import OneDayDataSetOut
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 import torch.nn as nn
+import torch.nn.functional as F
 import torch
 
 
 def pretrain_day(device, saved=False):
-    epoch = 100
+    epoch = 1000
     dataX, dataY = OneDayDataSetOut()
-    progress = tqdm(range(epoch))
+
     symbol = ['BTCUSDT',
               'ETHUSDT',
               'BNBUSDT',
@@ -27,10 +28,10 @@ def pretrain_day(device, saved=False):
     for i in range(len(dataX)):
 
         model = TrendReader(14, 13, 3).to(device)
- 
+        progress = tqdm(range(epoch))
         if saved:
             model.load_state_dict(torch.load('./model/oneday' + symbol[i] + '.pt'))
-        criterion = nn.MSELoss()
+
         optimizer = torch.optim.Adam(model.parameters(), lr=0.02)
         for _i in progress:
             batch_loss = 0.0
@@ -38,20 +39,19 @@ def pretrain_day(device, saved=False):
                 input = torch.from_numpy(dataX[i][t]).float().to(device)
                 output = torch.from_numpy(dataY[i][t]).float().to(device)
                 result = model.value(input, device)
-                loss = criterion(result, output).float().to(device)
+                loss = F.smooth_l1_loss(result, output).mean()
 
                 if not torch.isfinite(loss):
                     print(symbol[i] + "loss has outed none finited")
 
                 else:
                     loss.backward()
-                    nn.utils.clip_grad_norm_(model.parameters(), 0.5)
+                    nn.utils.clip_grad_norm_(model.parameters(), 0.2)
                     optimizer.step()
                     batch_loss += loss
-
             progress.set_description("loss: {:0.6f}".format(batch_loss / dataX[i].shape[0]))
         torch.save(model.state_dict(), './model/oneday' + symbol[i] + '.pt')
 
 
 if __name__ == "__main__":
-    pretrain_day('cpu')
+    pretrain_day('cuda')
