6a2ac2a9ba9aef61dbf279aaa2ad8dc12ec8b888
jepetolee <jepetolee@gmail.com>
2022-04-02 17:48:16 +0900

    changed updating model

--- TradeAlgorithm/update_csv.py
+++ TradeAlgorithm/update_csv.py
@@ -71,37 +71,26 @@ def update_future_15min_csv():
             pass
 
 
-def update_future_1min_csv():
+def update_future_1min_csv(symbol):
     client = Client(api_key="", api_secret="")
-    symbol = ['BTCUSDT',
-              'ETHUSDT',
-              'BNBUSDT',
-              'SOLUSDT',
-              'XRPUSDT',
-              'LUNAUSDT',
-              'BCHUSDT',
-              'WAVESUSDT',
-              'TRXUSDT',
-              'AVAXUSDT',
-              'LTCUSDT',
-              'NEARUSDT']
-    for i in trange(len(symbol)):
-        url = 'D:/CRYPTONALYTICS/TradeAlgorithm/csv/future/1min/1min_data_' + symbol[i] + ".csv"
-        try:
-            data = pd.read_csv(url)
-            data_time = data['0'].copy().to_numpy().astype('datetime64[ms]')
+    url = 'D:/CRYPTONALYTICS/TradeAlgorithm/csv/future/1min/1min_data_' + symbol + ".csv"
+    try:
+        data = pd.read_csv(url)
+        data_time = data['0'].copy().to_numpy().astype('datetime64[ms]')
 
-            day = pd.DataFrame(
-                client.futures_historical_klines(symbol=symbol[i], interval='1m', start_str=str(data_time[-1]),
-                                                 limit=1000)).drop(index=0, axis=0)
+        day = pd.DataFrame(
+            client.futures_historical_klines(symbol=symbol, interval='1m', start_str=str(data_time[-1]),
+                                             limit=1000)).drop(index=0, axis=0)
 
-            day.to_csv('./temp.csv')
-            day = pd.read_csv('./temp.csv')
-            data.drop(['Unnamed: 0'], axis=1, inplace=True)
-            day.drop(['Unnamed: 0'], axis=1, inplace=True)
-            data = pd.concat([data, day], ignore_index=True)
+        day.to_csv('./temp.csv')
+        day = pd.read_csv('./temp.csv')
+        data.drop(['Unnamed: 0'], axis=1, inplace=True)
+        day.drop(['Unnamed: 0'], axis=1, inplace=True)
+        data = pd.concat([data, day], ignore_index=True)
 
-            data.to_csv(url)
+        data.to_csv(url)
 
-        except FileNotFoundError:
-            pass
+    except FileNotFoundError:
+        pass
+
+
--- TrainingModel/Trader.py
+++ TrainingModel/Trader.py
@@ -8,14 +8,14 @@ import gc
 from torch.distributions import Categorical
 
 
-def TrainingTrader(symbol,number, device, saved=False):
+def TrainingTrader(symbol, device, saved=False):
     leverage, determine, position = Leverage(50, device=device).to(device), Determiner(50, device=device).to(device), \
                                     PositionDecisioner(50, device=device).to(device)
     if saved:
         leverage.load_state_dict('./model/' + symbol + '_leverage.pt')
         determine.load_state_dict('./model/' + symbol + '_determine.pt')
         position.load_state_dict('./model/' + symbol + '_position.pt')
-    update_future_1min_csv()
+    update_future_1min_csv(symbol)
     dataset = TradeDataSetOut()
     dataset_train = dataset[number]
     del dataset
@@ -26,4 +26,4 @@ def TrainingTrader(symbol,number, device, saved=False):
             print("도랄팍쥐 화이팅")
 
 
-TrainingTrader('BTCUSDT',0,'cpu')
+TrainingTrader('BTCUSDT','cpu')
