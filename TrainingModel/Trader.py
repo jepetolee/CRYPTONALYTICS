e24ce33ac48e96fb3a778888ac59b54f40019e74
jepetolee <jepetolee@gmail.com>
2022-04-06 06:40:52 +0900

    train

--- TrainingModel/Trader.py
+++ TrainingModel/Trader.py
@@ -1,4 +1,8 @@
-from ValueCrypto import PositionDecisioner, Determiner, Leverage
+import numpy as np
+import sys
+
+#sys.path.append('..')
+from ValueCrypto import PositionDecisioner
 from TradeAlgorithm import CurrentDataOut, TradeDataSetOut, update_future_1min_csv
 from tqdm import tqdm, trange
 import torch.nn as nn
@@ -9,13 +13,11 @@ from torch.distributions import Categorical
 
 
 def TrainingTrader(symbol, device, saved=False, grad_lock=False):
-    leverage, determines, position = Leverage(50, device=device).to(device), Determiner(50, device=device).to(device), \
-                                     PositionDecisioner(50, device=device).to(device)
+    position = PositionDecisioner(50, device=device).to(device)
 
     if saved:
-        leverage.load_state_dict('./model/' + symbol + '_leverage.pt')
-        determines.load_state_dict('./model/' + symbol + '_determine.pt')
         position.load_state_dict('./model/' + symbol + '_position.pt')
+
     update_future_1min_csv(symbol)
     dataset = TradeDataSetOut(symbol)
 
@@ -26,20 +28,24 @@ def TrainingTrader(symbol, device, saved=False, grad_lock=False):
 
     for _i in trange(epoch):
         total_score = 0.0
-        s = torch.from_numpy(dataset[0]).float().to(device).reshape(1, -1, 1)
-        for t in range(dataset.shape[0] - 1):
-            t = t + 1
-            s_prime = torch.from_numpy(dataset[t]).float().to(device).reshape(1, -1, 1)
+        s_o = torch.from_numpy(dataset[0]).float().to(device).reshape(1, -1, 1)
+        s = torch.from_numpy(np.gradient(dataset[0])).float().to(device).reshape(1, -1, 1)
 
+        position_keeper = 10
+        for t in range(dataset.shape[0]):
+            t = t
+
+            s_prime = torch.from_numpy(np.gradient(dataset[t])).float().to(device).reshape(1, -1, 1)
+            s_prime_o = torch.from_numpy(dataset[t]).float().to(device).reshape(1, -1, 1)
             if selecter:
-                leverage_t = leverage.setleverage(s).reshape(-1)
+                leverage_t = position.setleverage(s).reshape(-1)
                 position_t = position.setposition(s).reshape(-1)
                 position_a = Categorical(position_t).sample().item()
                 leverage_a = Categorical(leverage_t).sample().item()
                 leveragen = leverage_a + 1
 
                 position_prob = position_t[position_a]
-                selected_price = s[0][49].item()
+                selected_price = s_o[0][49].item()
                 position_s_prime = s_prime
                 leverage_prob = leverage_t[leverage_a]
                 position_s = s
@@ -47,45 +53,68 @@ def TrainingTrader(symbol, device, saved=False, grad_lock=False):
                 if position_a == 0:
                     selecter = False
                     position_v = 'LONG'
+                    best_reward = 0
 
                 elif position_a == 1:
                     selecter = False
                     position_v = 'SHORT'
-
+                    best_reward = 0
                 else:
                     position_v = 'NONE'
-                print(str(t) + "th," + str(position_v))
+                    best_reward = 0
 
-                if position_v == 'LONG':
-                    reward = (-100 + 100 * s[0][49].item() / selected_price) * leveragen
+                print(str(t) + "th," + str(position_v), position_a)
 
-                elif position_v == 'SHORT':
-                    reward = (100 - 100 * s[0][49].item() / selected_price) * leveragen
-                else:
-                    reward = 0
-                total_score += reward
+            if position_v == 'LONG':
+                reward = (-100 + 100 * s_o[0][49].item() / selected_price) * leveragen
+                reward_original = reward
+
+            elif position_v == 'SHORT':
+                reward = (100 - 100 * s_o[0][49].item() / selected_price) * leveragen
+                reward_original = reward
+            else:
+                reward = -0.2
+
+            if reward > best_reward:
+                best_reward = reward
+            else:
+                reward_original = reward
+                reward -= best_reward
+
+            if reward < -2 or reward_original < -1:
+                reward -= 0.4
+                print("position changed_intentionally")
+                selecter = True
 
             if position_v is not 'NONE':
-                determiner = determines.determine(s).reshape(-1)
+                determiner = position.determine(s).reshape(-1)
                 determined = Categorical(determiner).sample().item()
                 determined_prob = determiner[determined]
                 if determined == 0:
-                    print("position changed")
-                    selecter = True
-                determines.train_model(s, s_prime, determined, determined_prob, reward)
+                    if position_keeper == 0:
+                        total_score += reward_original
+                        print("position changed")
+                        reward -= 0.4
+                        selecter = True
+                        position_keeper = 10
+                    else:
+                        position_keeper -= 1
+                        print("position changed but intended to keep it.")
+
+                position.train_model_d(s, s_prime, determined, determined_prob, 10 * reward)
 
             if selecter:
-                position.train_model(position_s, position_s_prime, position_a, position_prob, reward)
-                leverage.train_model(position_s, position_s_prime, leverage_a, leverage_prob, reward)
+                position.train_model_l(position_s, position_s_prime, position_a, position_prob, 5 * reward_original)
+                position.train_model_p(position_s, position_s_prime, position_a, position_prob, 5 * reward_original)
 
             s = s_prime
-            print("reward is " + str(reward))
+            s_o = s_prime_o
+
+            print(str(position_v) + " " + str(leveragen) + ": reward is " + str(reward_original) + " " + str(t))
 
         print(total_score)
         if best_score < total_score:
             best_score = total_score
-            torch.save(leverage.state_dict(), './model/' + symbol + '_leverage.pt')
-            torch.save(determine.state_dict(), './model/' + symbol + '_determine.pt')
             torch.save(position.state_dict(), './model/' + symbol + '_position.pt')
 
 
--- TrainingModel/ValueCrypto.py
+++ TrainingModel/ValueCrypto.py
@@ -31,8 +31,10 @@ class PositionDecisioner(nn.Module):
         super(PositionDecisioner, self).__init__()
         self.incode = incode
         self.device = device
-        self.tencoder = Transformer(incode, 60, 1, 512, 8, 4, 0.1)
+        self.tencoder = Transformer(incode, 60, 1, 128, 8, 4, 0.1)
         self.q = nn.Linear(60, 3)
+        self.determiner = nn.Linear(60, 2)
+        self.leverage = nn.Linear(60, 20)
         self.v = nn.Linear(60, 1)
         self.optimizer = torch.optim.Adam(self.parameters(), lr=0.03)
 
@@ -44,45 +46,6 @@ class PositionDecisioner(nn.Module):
         torch.cuda.empty_cache()
         x = self.q(x)
         return func.softmax(x, dim=softmax_dim)
-
-    def value(self, x):
-        src_mask = self.tencoder.generate_square_subsequent_mask(x.shape[1]).to(self.device)
-        x = func.relu(self.tencoder(x, src_mask))
-        del src_mask
-        gc.collect()
-        torch.cuda.empty_cache()
-        return func.elu(self.v(x))
-
-    def train_model(self, s, s_prime, a, prob_a, reward, gamma=0.98, tau=0.97, weight=0.87,
-                    eps_clip=0.1):
-        for k in range(10):
-            td_target = reward + gamma * self.value(s_prime)
-            value = self.value(s)
-            delta = (td_target - value).detach()
-            pi = self.setposition(s)
-            pi_a = pi.gather(1, torch.tensor([a], dtype=torch.int64).reshape(-1, 1))
-            ratio = torch.exp(torch.log(pi_a) - torch.log(prob_a))
-            surr1 = ratio * delta
-            surr2 = torch.clamp(ratio, 1 + eps_clip, 1 - eps_clip) * delta
-            lossL = -torch.min(surr1, surr2).mean().item() + func.smooth_l1_loss(value, td_target) * weight
-            if not torch.isfinite(lossL):
-                print("loss has not finited")
-            else:
-                #    nn.utils.clip_grad_norm_(leverage.parameters(), 0.5)
-                self.optimizer.zero_grad()
-                lossL.backward()
-                self.optimizer.step()
-
-
-class Determiner(nn.Module):
-    def __init__(self, incode, device):
-        super(Determiner, self).__init__()
-        self.incode = incode
-        self.device = device
-        self.tencoder = Transformer(incode, 60, 1, 512, 16, 8, 0.1)
-        self.determiner = nn.Linear(60, 2)
-        self.v = nn.Linear(60, 1)
-        self.optimizer = torch.optim.Adam(self.parameters(), lr=0.03)
 
     def determine(self, x, softmax_dim=1):
         src_mask = self.tencoder.generate_square_subsequent_mask(x.shape[1]).to(self.device)
@@ -101,57 +64,37 @@ class Determiner(nn.Module):
         torch.cuda.empty_cache()
         return func.elu(self.v(x))
 
-    def train_model(self, s, s_prime, determined, determined_prob, reward, gamma=0.98, tau=0.97, weight=0.87,
-                    eps_clip=0.1):
-        for k in range(3):
-            td_target = reward + gamma * self.value(s_prime)
-            value = self.value(s)
-            delta = (td_target - value).detach()
-            pi = self.determine(s)
-
-            pi_a = pi.gather(1, torch.tensor([determined], dtype=torch.int64).reshape(-1, 1))
-            ratio = torch.exp(torch.log(pi_a) - torch.log(determined_prob))
-            surr1 = ratio * delta
-            surr2 = torch.clamp(ratio, 1 + eps_clip, 1 - eps_clip) * delta
-            lossD = -torch.min(surr1, surr2).mean().item() + func.smooth_l1_loss(value, td_target) * weight
-            if not torch.isfinite(lossD):
-                print("loss has not finited")
-            else:
-                self.optimizer.zero_grad()
-                lossD.backward()
-                self.optimizer.step()
-            # nn.utils.clip_grad_norm_(self.parameters(), 0.5)
-
-
-class Leverage(nn.Module):
-    def __init__(self, incode, device):
-        super(Leverage, self).__init__()
-        self.incode = incode
-        self.device = device
-        self.tencoder = Transformer(incode, 60, 1, 512, 16, 8, 0.1)
-        self.determine = nn.Linear(60, 2)
-        self.v = nn.Linear(60, 1)
-        self.optimizer = torch.optim.Adam(self.parameters(), lr=0.03)
-
     def setleverage(self, x, softmax_dim=1):
         src_mask = self.tencoder.generate_square_subsequent_mask(x.shape[1]).to(self.device)
         x = func.relu(self.tencoder(x, src_mask))
         del src_mask
         gc.collect()
         torch.cuda.empty_cache()
-        x = self.determine(x)
+        x = self.leverage(x)
         return func.softmax(x, dim=softmax_dim)
 
-    def value(self, x):
-        src_mask = self.tencoder.generate_square_subsequent_mask(x.shape[1]).to(self.device)
-        x = func.relu(self.tencoder(x, src_mask))
-        del src_mask
-        gc.collect()
-        torch.cuda.empty_cache()
-        return func.elu(self.v(x))
+    def train_model_p(self, s, s_prime, a, prob_a, reward, gamma=0.98, tau=0.97, weight=0.87,
+                      eps_clip=0.1):
+        for k in range(10):
+            td_target = reward + gamma * self.value(s_prime)
+            value = self.value(s)
+            delta = (td_target - value).detach()
+            pi = self.setposition(s)
+            pi_a = pi.gather(1, torch.tensor([a], dtype=torch.int64).reshape(-1, 1))
+            ratio = torch.exp(torch.log(pi_a) - torch.log(prob_a))
+            surr1 = ratio * delta
+            surr2 = torch.clamp(ratio, 1 + eps_clip, 1 - eps_clip) * delta
+            lossL = -torch.min(surr1, surr2).mean().item() + func.smooth_l1_loss(value, td_target) * weight
+            if not torch.isfinite(lossL):
+                print("loss has not finited")
+            else:
+                #    nn.utils.clip_grad_norm_(leverage.parameters(), 0.5)
+                self.optimizer.zero_grad()
+                lossL.backward()
+                self.optimizer.step()
 
-    def train_model(self, s, s_prime, a, prob_a, reward, gamma=0.98, tau=0.97, weight=0.87,
-                    eps_clip=0.1):
+    def train_model_l(self, s, s_prime, a, prob_a, reward, gamma=0.98, tau=0.97, weight=0.87,
+                      eps_clip=0.1):
         for k in range(10):
             td_target = reward + gamma * self.value(s_prime)
             value = self.value(s)
@@ -165,8 +108,27 @@ class Leverage(nn.Module):
             if not torch.isfinite(lossL):
                 print("loss has not finited")
             else:
+                #    nn.utils.clip_grad_norm_(leverage.parameters(), 0.5)
+                self.optimizer.zero_grad()
+                lossL.backward()
+                self.optimizer.step()
 
-                # nn.utils.clip_grad_norm_(self .parameters(), 0.5)
+    def train_model_d(self, s, s_prime, a, prob_a, reward, gamma=0.98, tau=0.97, weight=0.87,
+                      eps_clip=0.1):
+        for k in range(3):
+            td_target = reward + gamma * self.value(s_prime)
+            value = self.value(s)
+            delta = (td_target - value).detach()
+            pi = self.determine(s)
+            pi_a = pi.gather(1, torch.tensor([a], dtype=torch.int64).reshape(-1, 1))
+            ratio = torch.exp(torch.log(pi_a) - torch.log(prob_a))
+            surr1 = ratio * delta
+            surr2 = torch.clamp(ratio, 1 + eps_clip, 1 - eps_clip) * delta
+            lossL = -torch.min(surr1, surr2).mean().item() + func.smooth_l1_loss(value, td_target) * weight
+            if not torch.isfinite(lossL):
+                print("loss has not finited")
+            else:
+                #    nn.utils.clip_grad_norm_(leverage.parameters(), 0.5)
                 self.optimizer.zero_grad()
                 lossL.backward()
                 self.optimizer.step()
